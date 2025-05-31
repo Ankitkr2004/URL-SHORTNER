416202255f5ebaa9660d6bbcedb6e9c66a534015
Ankit kumar <143319891+Ankitkr2004@users.noreply.github.com>
2025-05-31 16:15:17 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -33,9 +33,15 @@ app.get("/:id",redirectFromShortUrl)
 
 app.use(errorHandler)
 
-app.listen(3000,()=>{
-    connectDB()
-    console.log("Server is running on http://localhost:3000");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
+// app.listen(3000,()=>{
+//     connectDB()
+//     console.log("Server is running on http://localhost:3000");
+// })
 
 // GET - Redirection 
